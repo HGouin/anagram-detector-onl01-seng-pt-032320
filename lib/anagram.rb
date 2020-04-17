b5d93c2daa6ee9fdc35eea4f6af7d53b99003efb
Hannah Gouin <hannah.l.gouin@gmail.com>
2020-04-17 03:53:56 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -8,34 +8,9 @@ class Anagram
   def match(words)
     matched_words = []
     words.each do |word|
-      next if word.length != @word.length
-      word_letters = {}
-      word.split("").each do |letter|
-        if word_letters[letter] == nil
-          word_letters[letter] = 0
-        end
-        word_letters[letter] += 1
-      end
-
-      instance_word_letters = {}
-      @word.split("").each do |letter|
-        if instance_word_letters[letter] == nil
-          instance_word_letters[letter] = 0
-        end
-        instance_word_letters[letter] += 1
-      end
-
-      matches = true
-      instance_word_letters.each do |letter, count|
-        if word_letters[letter] != count
-          matches = false
-          break
-        end
-      end
-      if matches
+      if word.split("").sort.join == @word.split("").sort.join
         matched_words << word
       end
-
     end
     matched_words
   end
